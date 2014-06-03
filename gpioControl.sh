eb5bc977d0f627e9201db92b9fb897bfdfb722ad
Enbar <michal.mochnac@gmail.com>
2014-06-03 11:25:09 +0200

    Update gpioControl.sh
    added parameters "time" and "pump" and handling for them

--- gpioControl.sh
+++ gpioControl.sh
@@ -1,23 +1,58 @@
 #!/bin/sh
 
+#params: PUMP [in, out] | TIME [s]
+
 PUMP_OUT="7"
 PUMP_IN="8"
-ON="1"
-OFF="0"
+START="1"
+STOP="0"
+MAX_TIME_SEC=180
 
-param1=$1
-param2=$2
+pump=666
+time=0
 
+setTime=$2
+if (( setTime > MAX_TIME_SEC )); then
+  setTime=180
+  echo "WARNING! Set higher time than 180 seconds, is set to 180!"
+fi
 
-echo "set both pump run"
+case "$1" in 
+  in)  echo "room water pump set!"
+          pump=$PUMP_OUT
+          ;;
+          
+  out)  echo "balkony water pump set!"
+          pump=$PUMP_IN
+          ;;
+          
+  *)  echo "Unknown command!"
+          ;;
+esac
 
-#echo $PUMP_OUT > /sys/class/gpio/export
-#echo "out" > /sys/class/gpio/gpio7/direction
+if (( pump != 666 )); then 
+  echo $pump > /sys/class/gpio/export
+  echo "out" > /sys/class/gpio/gpio+$pump/direction
+  echo $START > /sys/class/gpio/gpio+$pump/value
+  
+  echo "pump is running..."
+  
+  for pc in $(seq 0  180); do
+    remainedTime=$(( setTime - pc ))
+    echo -ne "remained Time: $remainedTime   \\r"
+    sleep 1
+    if (( $remainedTime == 0 )) ; then
+      break
+    fi
+  done
 
-echo $1 > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio$1/direction
+  echo $STOP > /sys/class/gpio/gpio+$pump/value
+  echo $pump > /sys/class/gpio/unexport
 
-echo $2 > /sys/class/gpio/gpio$1/value
-#echo $ON > /sys/class/gpio/gpio8/value
+  echo "Pump is stopped!"
+else
+  echo "ERROR Unknown pump"
+fi
 
-echo "pump set"
+exit
+
