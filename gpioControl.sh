9c999b825352ebc9874dc8b647146864786b7a59
Enbar <michal.mochnac@gmail.com>
2014-06-04 21:30:03 +0000

    just next editions for working script

--- gpioControl.sh
+++ gpioControl.sh
@@ -7,22 +7,14 @@ START="1"
 STOP="0"
 MAX_TIME_SEC=180
 
-
-value=67
 pump=666
-#time=0
-
 setTime=0
-if [ -z "$2" ]; then
-  echo "no argument for time"
+
+if [ -n "$2" ]; then
+  setTime=$2
 fi
 
-setTime=$2
-echo $setTime $MAX_TIME_SEC
-
 if [ "$setTime" -gt "$MAX_TIME_SEC" ]; then
-#if (( $setTime > $MAX_TIME_SEC )); then
-#  setTime=180
   echo "WARNING! Set higher time than 180 seconds, is set to 180!"
 fi
 
@@ -34,36 +26,35 @@ case $1 in
   out)  echo "balkony water pump set!"
           pump=$PUMP_IN
           ;;
-  -h)  echo "params: <pump> (in, out) ; <time> (seconds)"
+  -h)  echo "params: "
+       echo "	<pump> (in, out)"
+       echo "	<time> (seconds)"
        ;;
           
-  *)  echo "Unknown command!"
+  *)  echo "No argument for pump!"
           ;;
 esac
 
-#if (( $pump != "666" )); then 
 if [ "$pump" != "666" ]; then
   echo $pump > /sys/class/gpio/export
   echo "out" > /sys/class/gpio/gpio$pump/direction
-  echo $START > /sys/class/gpio/gpio$pump/value
-  
+  echo $START > /sys/class/gpio/gpio$pump/value  
   echo "pump is running..."
+
   for pc in $(seq 0  $MAX_TIME_SEC); do
     remainedTime=$(( setTime - pc ))
     echo -ne "remained Time: $remainedTime\\r"
     sleep 1
-#    if (( $remainedTime == 0 )) ; then
-if [ "$remainedTime" -eq 0 ]; then 
+
+  if [ "$remainedTime" -eq 1 ]; then 
      break
-fi
+  fi
   done
 
   echo $STOP > /sys/class/gpio/gpio$pump/value
   echo $pump > /sys/class/gpio/unexport
 
   echo "Pump is stopped!"
-else
-  echo "ERROR Unknown pump"
 fi
 
 exit
