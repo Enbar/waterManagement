9dc2105958ad31790fb30728a1767ca03a6df749
Enbar <michal.mochnac@gmail.com>
2014-06-05 13:08:33 +0200

    colours to echo

--- gpioControl.sh
+++ gpioControl.sh
@@ -7,6 +7,10 @@ START="1"
 STOP="0"
 MAX_TIME_SEC=180
 
+red='\e[0;31m'
+green='\e[0;32m'
+NC='\e[0m' # No Color
+
 pump=666
 setTime=0
 
@@ -15,15 +19,15 @@ if [ -n "$2" ]; then
 fi
 
 if [ "$setTime" -gt "$MAX_TIME_SEC" ]; then
-  echo "WARNING! Set higher time than 180 seconds, is set to 180!"
+  echo -e "\e[0;31WARNING!\e[0m Set higher time than 180 seconds, is set to 180!"
 fi
 
 case $1 in 
-  in)  echo "room water pump set!"
+  in)  echo -e "${green}room${NC} water pump set!"
           pump=$PUMP_OUT
           ;;
           
-  out)  echo "balkony water pump set!"
+  out)  echo "${green}balkony${NC} water pump set!"
           pump=$PUMP_IN
           ;;
   -h)  echo "params: "
@@ -31,7 +35,7 @@ case $1 in
        echo "	<time> (seconds)"
        ;;
           
-  *)  echo "No argument for pump!"
+  *)  echo "${red}No argument for pump!"
           ;;
 esac
 
@@ -43,7 +47,7 @@ if [ "$pump" != "666" ]; then
 
   for pc in $(seq 0  $MAX_TIME_SEC); do
     remainedTime=$(( setTime - pc ))
-    echo -ne "Will be stopped in $remainedTime sec   \\r"
+    echo -ne "Will be stopped in ${green}$remainedTime sec   \\r"
     sleep 1
 
   if [ "$remainedTime" -eq 1 ]; then 
